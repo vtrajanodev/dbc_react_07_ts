d890a789fe5568f3708a69d8478bef308d4d2397
Victor Trajano <trajanodev@hotmail.com>
2021-11-24 11:30:14 -0400

    Criação dos hooks

---
+++ src/components/About/About.tsx
@@ -0,0 +1,30 @@
+import { useApi } from "../../hooks/useApi";
+import styles from '../../styles/home.module.scss'
+
+export const About = () => {
+
+  const { user } = useApi()
+
+  return (
+    <main>
+       {user?.map((user, index) => (
+        <div key={index}>
+          <div className="container">
+            <div>
+              <div className={styles.profilePicture}>
+                <img src={user.avatar_url} alt="foto perfil" />
+              </div>
+
+              <div className={styles.sobre}>
+                <h1>{user.name}</h1>
+                <h3>{user.login}</h3>
+                <p>{user.bio}</p>
+              </div>
+            </div>
+
+          </div>
+        </div>
+      ))}
+    </main>
+  );
+}
--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,42 +1,14 @@
+import { About } from '../../components/About/About';
+import { Repos } from '../../components/Repos/Repos';
 import { useApi } from '../../hooks/useApi';
-import styles from './home.module.scss'
+import styles from '../../styles/home.module.scss'
 
 export const Home = () => {
-
-  const { user, repo } = useApi()
-  
+ 
   return (
-    <main>
-      {user?.map((user, index) => (
-        <div key={index}>
-          <div className="container">
-            <div>
-              <div className={styles.profilePicture}>
-                <img src={user.avatar_url} alt="foto perfil" />
-              </div>
-
-              <div className={styles.sobre}>
-                <h1>{user.name}</h1>
-                <h3>{user.login}</h3>
-                <p>{user.bio}</p>
-              </div>
-            </div>
-
-          </div>
-        </div>
-      ))}
-      <h2>Repositórios:</h2>
-      <div className={styles.repos}>
-        {repo.map((repo, index) => (
-          <div key={index}>
-            <div className={styles.repoCard}>
-              <h4>{repo.name}</h4>
-              <p>{repo.description}</p>
-              <a href={repo.html_url}>Acesse o repositório no github</a>
-            </div>
-          </div>
-        ))}
-      </div>
-    </main>
+    <>
+     <About />
+     <Repos />
+    </>
   );
 }
